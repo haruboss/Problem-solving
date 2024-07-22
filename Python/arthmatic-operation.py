64f44867879f1f79255e692df11865224f736a75
Harshit <harshit.saxena2016@gmail.com>
2024-07-22 15:04:28 +0530

    revised arthmetic fn

--- Python/arthmatic-operation.py
+++ Python/arthmatic-operation.py
@@ -1,9 +1,14 @@
-a=int(input('Please Enter number1: '))
-b=int(input('Please Enter number2: '))
-
-if 1<=a<=(10**10) and 1<=b<=(10**10):
-    print("Addition of two num: ", a+b)
-    print("Subtraction of two num: ", a-b)
-    print("Multiplication of two num: ", a*b)
-else:
-    print('Please Enter valid number.')
+def arthmatic_operation():
+    num1 = float(input("Please Enter Number 1: "))
+    num2 = float(input("Please Enter Number 2: "))
+    
+    if number_is_valid(num1, num2):
+        return {'addition':num1 + num2, 'subtraction':num1 - num2, 'multiplication': num1 * num2 }
+    return "Please enter a valid number."
+    
+def number_is_valid(number1, number2):
+    return 1 <= number1 <= (10 ** 10) and 1 <= number2 <= (10 ** 10)
+   
+    
+result = arthmatic_operation()
+print(result)
