83d82f4b09ee1f9c4058b249b1005d3816584926
Harshit <harshit.saxena2016@gmail.com>
2024-07-22 14:38:35 +0530

    refactor add two numbers

--- Python/add-two-numbers.py
+++ Python/add-two-numbers.py
@@ -1,6 +1,8 @@
 def add_two_numbers():
-	n1=int(input("Enter Numer1: "))
-	n2=int(input("Enter Number2: "))
-	print(n1+n2)
-
-add_two_numbers()
+    num1 = int(input("Please Enter number 1: "))
+    num2 = int(input("Please Enter number 2: "))
+    
+    return f"Sum of {num1} and {num2} is {num1 + num2}."
+    
+result = add_two_numbers()
+print(result)
